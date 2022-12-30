e4e63045258290742b7119eb7f15e116db9de49e
crazyBaboon <nuno360@gmail.com>
2022-12-30 21:43:52 +0000

    update copyright year

--- Cairo_System_Dynamics_0.15.c
+++ Cairo_System_Dynamics_0.15.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C)2017-2018 Nuno Ferreira
+ * Copyright (C)2017-2022 Nuno Ferreira
  *
  * Cairo System Dynamics is  free software; you can redistribute  it and/or
  * modify it under the  terms of the GNU General  Public License as published
